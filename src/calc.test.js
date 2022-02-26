b1b4110a0fa4c2506162d7bce443aec4af316489
Rafael Andrade de Oliveira <eu.rafa@gmail.com>
2022-02-25 21:11:17 -0300

    fix: tests

--- src/calc.test.js
+++ src/calc.test.js
@@ -1,17 +1,17 @@
-const sum = require('./calc');
+const { sum, subtract, divide, multiply } = require('./calc');
 
 test("sum 2 + 2", () => {
     expect(sum(2, 2)).toBe(4);
 });
 
 test("subtract 2 - 2", () => {
-    expect(sum(2, 2)).toBe(0);
+    expect(subtract(2, 2)).toBe(0);
 });
 
 test("divide 2 / 2", () => {
-    expect(sum(2, 2)).toBe(1);
+    expect(divide(2, 2)).toBe(1);
 });
 
 test("multiply 2 * 2", () => {
-    expect(sum(2, 2)).toBe(4);
+    expect(multiply(2, 2)).toBe(4);
 });
